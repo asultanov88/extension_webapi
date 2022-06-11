42a7fea2c19a1cf3482b434bb70452905278fb18
CoolBean <uzsultanov@gmail.com>
2022-06-11 16:11:43 -0400

    intentional bug o test error message.

--- app/Http/Controllers/ClientEnvironmentsController.php
+++ app/Http/Controllers/ClientEnvironmentsController.php
@@ -142,7 +142,7 @@ class ClientEnvironmentsController extends Controller
 
         try {
             
-            $matchingEnvironment = Environment::where('clientId','=',$request['clientId'])
+            $matchingEnvironment = Environment::where('XclientId','=',$request['clientId'])
                                           ->whereRaw('LOWER(name) = LOWER(?)', ["{$request['name']}"])
                                           ->first();
 
